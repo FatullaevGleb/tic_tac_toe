62b1b50f45b980bc2c976903e6f53a02fa3d8997
FatullaevGleb <mr.gleb-fatullaev@yandex.ru>
2025-09-29 15:23:55 +0300

    add all staff

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -14,6 +14,25 @@ class Board:
             print('|'.join(row))
             print('-' * 5)
 
+    def is_board_full(self):
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                if self.board[i][j] == ' ':
+                    return False
+        return True
+
+    def check_win(self, player):
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+
+        return False
+
     def __str__(self):
         return (
             'Объект игрового поля размером '
